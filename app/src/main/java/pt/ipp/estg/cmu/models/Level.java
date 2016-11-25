6bb7353179ca3f01b5eeaac1bf93c6384e35f5ff
ivoribeiro <ivolopesribeiro15@gmail.com>
2016-11-25 11:32:11 +0000

    Models , libraria parar gestao base de dados

--- app/src/main/java/pt/ipp/estg/cmu/models/Level.java
+++ app/src/main/java/pt/ipp/estg/cmu/models/Level.java
@@ -6,6 +6,10 @@ package pt.ipp.estg.cmu.models;
 
 public class Level {
     private String number;
+    private int pontuacaoBase;
+    private int pontuacaoBaseErrada;
+    private int pontuacaoHint;
+
 
     public Level(String number) {
         this.number = number;
@@ -18,4 +22,31 @@ public class Level {
     public void setNumber(String number) {
         this.number = number;
     }
+
+
+    public int getPontuacaoBase() {
+        return pontuacaoBase;
+    }
+
+    public void setPontuacaoBase(int pontuacaoBase) {
+        this.pontuacaoBase = pontuacaoBase;
+    }
+
+    public int getPontuacaoBaseErrada() {
+        return pontuacaoBaseErrada;
+    }
+
+    public void setPontuacaoBaseErrada(int pontuacaoBaseErrada) {
+        this.pontuacaoBase = pontuacaoBaseErrada;
+    }
+
+    public int getPontuacaoHint() {
+        return pontuacaoHint;
+    }
+
+    public void setPontuacaoHint(int pontuacaoHint) {
+        this.pontuacaoHint = pontuacaoHint;
+    }
+
+
 }
---
+++ app/src/main/java/pt/ipp/estg/cmu/models/Pergunta.java
@@ -0,0 +1,20 @@
+package pt.ipp.estg.cmu.models;
+
+/**
+ * Created by ivoribeiro on 25-11-2016.
+ */
+
+public class Pergunta  {
+
+    private int nivel;
+    private String categoria;
+    private String imagem;
+    private int respostaCerta;
+    private int nRespostaCertas;
+    private int nRespostaErradas;
+    private boolean acertou;
+
+
+    public Pergunta() {
+    }
+}
