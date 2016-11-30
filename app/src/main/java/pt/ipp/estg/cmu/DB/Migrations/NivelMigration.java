b63d9a4b347ac32748be2ecff4b69660d9d4b0d5
ivoribeiro <ivolopesribeiro15@gmail.com>
2016-11-30 11:08:51 +0000

    db fields set

--- app/src/main/java/pt/ipp/estg/cmu/DB/Migrations/NivelMigration.java
+++ app/src/main/java/pt/ipp/estg/cmu/DB/Migrations/NivelMigration.java
@@ -5,6 +5,12 @@ public class NivelMigration extends Migration {
 
     public NivelMigration() {
         super("nivel");
+        this.addField(new Field(true, "numero", "VARCHAR(3)"));
+        this.addField(new Field(false, "nPerguntas", "NUMERIC"));
+        this.addField(new Field(false, "pontuacaoBase", "NUMERIC"));
+        this.addField(new Field(false, "pontuacaoBaseErrada", "NUMERIC"));
+        this.addField(new Field(false, "pontuacaoHint", "NUMERIC"));
+        this.addField(new Field(true, "nRespostasCertas", "NUMERIC"));
         this.setCreate(this.generateCreate());
         this.setUpgrade(this.generateUpgrade());
     }
