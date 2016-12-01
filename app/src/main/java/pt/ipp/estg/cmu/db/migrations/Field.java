179df0dcbffa2218d072c6625b853ee8a0c26cde
ivoribeiro <ivolopesribeiro15@gmail.com>
2016-12-01 12:25:50 +0000

    migration merge fix

--- app/src/main/java/pt/ipp/estg/cmu/db/migrations/Field.java
+++ app/src/main/java/pt/ipp/estg/cmu/db/migrations/Field.java
@@ -4,10 +4,12 @@ package pt.ipp.estg.cmu.db.migrations;
 public class Field {
     private String name;
     private boolean primary;
+    private boolean autoincrement;
     private String type;
 
-    public Field(boolean primary, String name, String type) {
+    public Field(boolean primary, boolean autoincrement, String name, String type) {
         this.primary = primary;
+        this.autoincrement = autoincrement;
         this.name = name;
         this.type = type;
     }
@@ -15,6 +17,7 @@ public class Field {
     @Override
     public String toString() {
         String primary = this.primary ? "PRIMARY KEY" : "";
-        return this.name + " " + this.type + primary;
+        String increment = this.autoincrement ? "autoincrement" : "";
+        return " "+ this.name + " " + this.type + " " + primary + " " + increment+",";
     }
 }
--- app/src/main/java/pt/ipp/estg/cmu/db/migrations/NivelMigration.java
+++ app/src/main/java/pt/ipp/estg/cmu/db/migrations/NivelMigration.java
@@ -5,12 +5,13 @@ public class NivelMigration extends Migration {
 
     public NivelMigration() {
         super("nivel");
-        this.addField(new Field(true, "numero", "VARCHAR(3)"));
-        this.addField(new Field(false, "nPerguntas", "NUMERIC"));
-        this.addField(new Field(false, "pontuacaoBase", "NUMERIC"));
-        this.addField(new Field(false, "pontuacaoBaseErrada", "NUMERIC"));
-        this.addField(new Field(false, "pontuacaoHint", "NUMERIC"));
-        this.addField(new Field(true, "nRespostasCertas", "NUMERIC"));
+        this.addField(new Field(true, true, "id", "NUMERIC"));
+        this.addField(new Field(false, false, "numero", "VARCHAR(3)"));
+        this.addField(new Field(false, false, "nPerguntas", "NUMERIC"));
+        this.addField(new Field(false, false, "pontuacaoBase", "NUMERIC"));
+        this.addField(new Field(false, false, "pontuacaoBaseErrada", "NUMERIC"));
+        this.addField(new Field(false, false, "pontuacaoHint", "NUMERIC"));
+        this.addField(new Field(true, false, "nRespostasCertas", "NUMERIC"));
         this.setCreate(this.generateCreate());
         this.setUpgrade(this.generateUpgrade());
     }
