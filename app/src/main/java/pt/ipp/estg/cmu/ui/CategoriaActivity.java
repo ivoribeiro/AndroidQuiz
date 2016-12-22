bca939a62e42e6f6160ec14b05b3818a92580204
Navega <navegaamorim@gmail.com>
2016-12-22 00:01:08 +0000

    Parcial Admin Auth Implementação

--- app/src/main/java/pt/ipp/estg/cmu/ui/CategoriaActivity.java
+++ app/src/main/java/pt/ipp/estg/cmu/ui/CategoriaActivity.java
@@ -1,12 +1,13 @@
 package pt.ipp.estg.cmu.ui;
 
+import android.app.Dialog;
 import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import pt.ipp.estg.cmu.R;
 import pt.ipp.estg.cmu.util.Util;
@@ -26,6 +27,42 @@ public class CategoriaActivity extends AppCompatActivity {
 
         isAdmin = getIntent().getBooleanExtra(Util.ARG_ADMIN, false);
 
+        if (isAdmin) {
+            buildDialog();
+        } else {
+            startFragmetCategoria();
+        }
+    }
+
+
+    private void buildDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_sign_in);
+        dialog.setTitle(getResources().getString(R.string.dialog_title));
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height = WindowManager.LayoutParams.MATCH_PARENT;
+
+        final EditText editText = (EditText) dialog.findViewById(R.id.edit_pin);
+
+        Button ok = (Button) dialog.findViewById(R.id.dialog_ok);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                System.out.println(editText.getText().toString());
+                startFragmetCategoria();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setAttributes(lp);
+    }
+
+
+    public void startFragmetCategoria() {
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.frame_layout, CategoriaFragment.newInstance(isAdmin))
